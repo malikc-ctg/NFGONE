aa583b5e5f6c0ffe65ab96631c971c948af187d2
Malik Campbell <malikcampbell@Mac-mini.local>
2026-06-05 01:21:40 -0400

    chore: redirect unused routes to landing page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path((?!admin|api|_next|favicon.ico|.*\\.).*)',
+        destination: '/',
+        permanent: false,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
